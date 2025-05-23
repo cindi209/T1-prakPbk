2c002f6b6c10e8e5f0f5e2b7acdf8760a2a9f9c5
cindi209 <cindilestari209@gmail.com>
2025-05-23 22:05:14 +0700

    penambahan bio data

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -13,10 +13,9 @@ const count = ref(0)
 
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
+    <p>Nama   : Nurainun cindi lestari</p>
+    <p>NPM    : 233510801</p>
+    <p>Kelas  : 4D </p>
   </div>
 
   <p>
